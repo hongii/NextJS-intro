3a1114d5b7e9bbe6e78e0edcd8704dec1cdae554
khe <khj778878@naver.com>
2022-09-29 00:56:52 +0900

    [add]./pages/_app.js를 생성하고 App component를 통해 page를 초기화하고 재정의 할 수 있다.

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import NavBar from "../components/NavBar";
+import "../styles/glogals.css";
+/*파일명.module.css 파일 형태를 제외한 모든 나머지 css파일들은 _app.js에서만 import해와서 사용해야 한다. 
+(글로벌 css간의 충돌을 피하기 위해서)*/
+
+export default function App({ Component, pageProps }) {
+	return (
+		<>
+			<NavBar />
+			<Component {...pageProps} />
+			<span>Hello, global</span>
+
+			{/* styled jsx를 사용한 global css style */}
+			<style jsx global>{`
+				a{
+					color: gray;
+				}
+			`}
+			</style>
+		</>
+	)
+}
