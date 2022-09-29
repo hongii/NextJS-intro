4beacf6310b0646f6102cff38c8108d14c1d2bf9
khe <khj778878@naver.com>
2022-09-29 22:44:01 +0900

    [add]Layout 컴포넌트 생성: _add.js에서 <Component {...pageProps} />를 감싸준다.

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import NavBar from "../components/NavBar";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 /*파일명.module.css 파일 형태를 제외한 모든 나머지 css파일들은 _app.js에서만 import해와서 사용해야 한다. 
 (글로벌 css간의 충돌을 피하기 위해서)*/
@@ -6,10 +6,10 @@ import "../styles/globals.css";
 export default function App({ Component, pageProps }) {
 	return (
 		<>
-			<NavBar />
-			<Component {...pageProps} />
-			<span>Hello, global</span>
-
+			<Layout>
+				<Component {...pageProps} />
+				<span>Hello, global</span>
+			</Layout>
 			{/* styled jsx를 사용한 global css style */}
 			<style jsx global>{`
 				a{
