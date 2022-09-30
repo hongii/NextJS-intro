8fb2461662167acf77d0c27f5775a6f82ce28774
khe <khj778878@naver.com>
2022-09-30 13:25:50 +0900

    [change]NavBar CSS변경

--- components/NavBar.js
+++ components/NavBar.js
@@ -6,14 +6,18 @@ export default function NavBar() {
 	const router = useRouter();
 
 	return (
-		<nav >
-			<Link href="/">
-				<a className={`${styles.link} ${router.pathname === "/" ? styles.active : ""}`}>Home</a>
-			</Link>
+		<nav className={styles.nav}>
+			{/* public 폴더에 있는 파일은 "/파일명" 으로 바로 사용 가능하다. (../public/vercel.svg 와 같이 경로명x)*/}
+			<img className={ styles.image} src="/vercel.svg" alt="image"/>
+			<div className={styles.linkContainer}>
+				<Link href="/">
+					<a className={`${router.pathname === "/" ? styles.active : ""}`}>Home</a>
+				</Link>
 
-			<Link href="/about">
-				<a className={`${styles.link} ${router.pathname === "/about" ? styles.active : ""}`}>About</a>
-			</Link>
+				<Link href="/about">
+					<a className={`${router.pathname === "/about" ? styles.active : ""}`}>About</a>
+				</Link>
+			</div>
 		</nav>
 	)
 }
