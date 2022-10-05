077f2da12f28104c0f47f790884d305058e5043b
khe <khj778878@naver.com>
2022-10-06 02:48:32 +0900

    [Add]: pages/index.js에 SSR방식으로 실행하는 함수 구현

--- pages/index.js
+++ pages/index.js
@@ -43,3 +43,44 @@ export default function Home() {
 		</div>
 	)
 }
+
+
+/*
+* next.js의 server-side-renderring
+
+export default function Home({ movies}) {
+	return (
+		<div className={styles.container}>
+			<Seo title="Home" />
+			{
+				movies?.map((movie) => {
+					if (movie.poster_path !== null) {
+						return (
+							<div className={styles.movie} key={movie.id} >
+								<img src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} />
+								<h4>{movie.original_title}</h4>
+							</div>
+						)
+					}
+				})
+			}
+		</div>
+	)
+}
+
+//함수이름 불변 => getServerSideProps라는 함수명은 절대 바꾸면 안됨
+//아래 코드는 client가 아니라 server에서 동작한다. => 따라서, API_KEY룰 숨길수도 있다. 
+//loading화면 없이 API요청에 대한 응답이 완료되길 기다렸다가 모든 정보를 화면에 보여준다.
+//javascript를 비활성해도 아무문제 없이 동작한다.
+export async function getServerSideProps() {
+	const response = await axios.get("http://localhost:3000/api/movies");
+	
+	//return되는 값은 Home컴포넌트의 props로 들어간다.
+	return {
+		props: {
+			movies: response.data.results
+		}
+	}
+}
+
+ */
