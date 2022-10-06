817cfd2c756a059ca3b32186327382b52ec170c3
khe <khj778878@naver.com>
2022-10-06 16:03:14 +0900

    [Add]: url = localhost:3000/movies/all로 들어가고 싶다면, pages폴더에 movies폴더를 생성하고 all.js를 생성한다. /movies로 들어가고 싶다면 movies폴더에 index.js 파일을 생성한다.

---
+++ pages/movies/all.js
@@ -0,0 +1,7 @@
+/* URL : localhost:3000/movies/all로 접속시 이 페이지가 뜬다. */
+
+export default function all() {
+	return (
+		<div>all</div>
+	)
+}
---
+++ pages/movies/index.js
@@ -0,0 +1,7 @@
+/* URL : localhost:3000/movies로 접속시 이 페이지가 뜬다. */
+
+export default function movies() {
+	return (
+		<div>movies index.js</div>
+	)
+}
